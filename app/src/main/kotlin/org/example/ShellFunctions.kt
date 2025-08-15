157208220380f259ffe3f7912a0062aaccdc76da
Mounir Nessim <mmnessim@gmail.com>
2025-08-14 21:15:06 -0400

    add some doc comments

--- app/src/main/kotlin/org/example/ShellFunctions.kt
+++ app/src/main/kotlin/org/example/ShellFunctions.kt
@@ -2,7 +2,20 @@ package org.example
 
 import java.io.File
 
+/**
+ * All shell builtin recreations live in this class
+ *
+ * TODO pass @param cwd to all functions and @return Pair<String, String>
+ * with both the function output and the cwd (to handle cases where it changes)
+ * This would alter the logic in the repl loop in App.kt
+ */
 class ShellFunctions {
+
+    /**
+     * cat builtin function
+     * @param p arguments and flags
+     * TODO handle multiple arguments
+     */
     fun cat(p: ParsedInput): String {
         if (p.arguments.isEmpty()) {
             return ""
@@ -14,7 +27,6 @@ class ShellFunctions {
         val showEnds = flagSet.contains('E')
 
         try {
-
             var contents = ""
             var lineNo = 1
             File(p.arguments[0]).useLines() { lines ->
@@ -33,18 +45,27 @@ class ShellFunctions {
                     contents += outputLine + "\n"
                 }
             }
-
             return contents
         } catch (e: Exception) {
             return "Error: ${e.message}"
         }
     }
 
+    /**
+     * Simply displays my chess stats, not really related to the rest of the project
+     */
     fun stats(): String {
         Chess().getStats()
         return ""
     }
 
+    /**
+     * Recreation of ls builtin
+     * @param cwd is required in order to display the right output after changing directory
+     * @param p the arguments and flags
+     *
+     * Uses ASCII green for directories "\u001B[32m"
+     */
     fun ls(cwd: String, p: ParsedInput): String {
         val filePath = if (p.arguments.isNotEmpty()) p.arguments[0] else cwd
         try {
@@ -69,6 +90,12 @@ class ShellFunctions {
         }
     }
 
+    /**
+     * cd builtin function
+     * @param cwd is required because Kotlin does not really change directories
+     * @param p arguments and flags
+     * @return the new cwd to display in prompt and pass to other functions as needed
+     */
     fun cd(cwd: String, p: ParsedInput): String {
         val newDir = File(cwd, p.arguments[0]).canonicalFile
 
